c00b87fedd9416c4f64932a23503cd12e3d8ca1a
Waza <waza@Tammy-MacBook-Pro.local>
2025-11-07 12:24:22 +0100

    fix: reeduced linee height on hero text

--- app/components/Herobanner.tsx
+++ app/components/Herobanner.tsx
@@ -32,7 +32,7 @@ const Herobanner = () => {
 
             <div className="py-5">
               <p
-                className="text-5xl lg:text-7xl leading-tight text-white"
+                className="text-5xl lg:text-7xl leading-tighter  text-white"
                 style={{ fontFamily: "var(--font-instrument-serif)" }}
               >
                 Software Developer / Frontend Engineer
